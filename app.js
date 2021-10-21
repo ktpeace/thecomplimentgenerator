2eb3cd9f00d465f525dbe0b0f0279dc112e9e97b
Kitty <75002769+ktpeace@users.noreply.github.com>
2021-10-20 21:32:10 -0500

    Initial files

---
+++ app.js
@@ -0,0 +1,247 @@
+var arr = [];
+
+while (arr.length < 4) {
+  var r = Math.floor(Math.random() * 18) + 1;
+  if (arr.indexOf(r) === -1) arr.push(r);
+}
+
+switch (arr[0]) {
+  case 1:
+    document.querySelector(".compliment1").innerHTML = "Honeybuns,";
+    break;
+  case 2:
+    document.querySelector(".compliment1").innerHTML = "Champ:";
+    break;
+  case 3:
+    document.querySelector(".compliment1").innerHTML = "Fact:";
+    break;
+  case 4:
+    document.querySelector(".compliment1").innerHTML = "Dang...";
+    break;
+  case 5:
+    document.querySelector(".compliment1").innerHTML = "Ace,";
+    break;
+  case 6:
+    document.querySelector(".compliment1").innerHTML = "Hear ye, hear ye,";
+    break;
+  case 7:
+    document.querySelector(".compliment1").innerHTML = "Know this:";
+    break;
+  case 8:
+    document.querySelector(".compliment1").innerHTML = "Experts agree:";
+    break;
+  case 9:
+    document.querySelector(".compliment1").innerHTML = "Listen to me! LISTEN TO ME GODDAMMIT!!!";
+    break;
+  case 10:
+    document.querySelector(".compliment1").innerHTML = "Starlet,";
+    break;
+  case 11:
+    document.querySelector(".compliment1").innerHTML = "Baby grrrrl,";
+    break;
+  case 12:
+    document.querySelector(".compliment1").innerHTML = "Hey LADY!";
+    break;
+  case 13:
+    document.querySelector(".compliment1").innerHTML = "Tiger,";
+    break;
+  case 14:
+    document.querySelector(".compliment1").innerHTML = "Oh captain my captain,";
+    break;
+  case 15:
+    document.querySelector(".compliment1").innerHTML = "Sweetmeat,";
+    break;
+  case 16:
+    document.querySelector(".compliment1").innerHTML = "Clean out your ears!";
+    break;
+  case 17:
+    document.querySelector(".compliment1").innerHTML = "Duckling,";
+    break;
+  case 18:
+    document.querySelector(".compliment1").innerHTML = "HOOO-WEE!";
+    break;
+  default:
+    console.log("Error: " + arr[0]);
+}
+
+switch (arr[1]) {
+  case 1:
+    document.querySelector(".compliment2").innerHTML = "the mere notion of you";
+    break;
+  case 2:
+    document.querySelector(".compliment2").innerHTML = "your soul";
+    break;
+  case 3:
+    document.querySelector(".compliment2").innerHTML = "your hair";
+    break;
+  case 4:
+    document.querySelector(".compliment2").innerHTML = "everything you do";
+    break;
+  case 5:
+    document.querySelector(".compliment2").innerHTML = "your sassy Sasquatch smile";
+    break;
+  case 6:
+    document.querySelector(".compliment2").innerHTML = "every thought you have";
+    break;
+  case 7:
+    document.querySelector(".compliment2").innerHTML = "the flesh of your flesh";
+    break;
+  case 8:
+    document.querySelector(".compliment2").innerHTML = "what you got goin' on";
+    break;
+  case 9:
+    document.querySelector(".compliment2").innerHTML = "your life's journey";
+    break;
+  case 10:
+    document.querySelector(".compliment2").innerHTML = "your DNA";
+    break;
+  case 11:
+    document.querySelector(".compliment2").innerHTML = "that big brain of yours";
+    break;
+  case 12:
+    document.querySelector(".compliment2").innerHTML = "whatever your secret is";
+    break;
+  case 13:
+    document.querySelector(".compliment2").innerHTML = "your roll, dinner or behavioral,";
+    break;
+  case 14:
+    document.querySelector(".compliment2").innerHTML = "the way you make penguins dance";
+    break;
+  case 15:
+    document.querySelector(".compliment2").innerHTML = "your impact on climate change";
+    break;
+  case 16:
+    document.querySelector(".compliment2").innerHTML = "your rockin' bod";
+    break;
+  case 17:
+    document.querySelector(".compliment2").innerHTML = "every alien lifeform you host";
+    break;
+  case 18:
+    document.querySelector(".compliment2").innerHTML = "the fluffiest bathrobe you own";
+    break;
+  default:
+    console.log("Error: " + arr[1]);
+}
+
+switch (arr[2]) {
+  case 1:
+    document.querySelector(".compliment3").innerHTML = "makes birds sing,";
+    break;
+  case 2:
+    document.querySelector(".compliment3").innerHTML = "has serious game,";
+    break;
+  case 3:
+    document.querySelector(".compliment3").innerHTML = "deserves the Nobel Prize,";
+    break;
+  case 4:
+    document.querySelector(".compliment3").innerHTML = "is the talk of the animal kingdom,";
+    break;
+  case 5:
+    document.querySelector(".compliment3").innerHTML = "pays off like a mantis prays,";
+    break;
+  case 6:
+    document.querySelector(".compliment3").innerHTML = "just shimmers,";
+    break;
+  case 7:
+    document.querySelector(".compliment3").innerHTML = "is a national treasure,";
+    break;
+  case 8:
+    document.querySelector(".compliment3").innerHTML = "roars like a lion,";
+    break;
+  case 9:
+    document.querySelector(".compliment3").innerHTML = "dries up rainclouds,";
+    break;
+  case 10:
+    document.querySelector(".compliment3").innerHTML = "is a rainbow factory,";
+    break;
+  case 11:
+    document.querySelector(".compliment3").innerHTML = "is like unicorn hopscotch,";
+    break;
+  case 12:
+    document.querySelector(".compliment3").innerHTML = "is made of diamonds,";
+    break;
+  case 13:
+    document.querySelector(".compliment3").innerHTML = "should be taught in school,";
+    break;
+  case 14:
+    document.querySelector(".compliment3").innerHTML = "is like that part in Little House where they pour molasses on snow to make candy,";
+    break;
+  case 15:
+    document.querySelector(".compliment3").innerHTML = "--JUST--UNBELIEVABLE! FUCK!!!";
+    break;
+  case 16:
+    document.querySelector(".compliment3").innerHTML = "turns all the haters to lovers,";
+    break;
+  case 17:
+    document.querySelector(".compliment3").innerHTML = "curls every twirly-whirl,";
+    break;
+  case 18:
+    document.querySelector(".compliment3").innerHTML = "tastes like cotton candy and saltwater taffy,";
+    break;
+  default:
+    console.log("Error: " + arr[2]);
+}
+
+switch (arr[3]) {
+  case 1:
+    document.querySelector(".compliment4").innerHTML = "homo sa-pi-en!";
+    break;
+  case 2:
+    document.querySelector(".compliment4").innerHTML = "24/5.";
+    break;
+  case 3:
+    document.querySelector(".compliment4").innerHTML = "can I get an amen?";
+    break;
+  case 4:
+    document.querySelector(".compliment4").innerHTML = "so treat yoself.";
+    break;
+  case 5:
+    document.querySelector(".compliment4").innerHTML = "that's just science.";
+    break;
+  case 6:
+    document.querySelector(".compliment4").innerHTML = "mic drop.";
+    break;
+  case 7:
+    document.querySelector(".compliment4").innerHTML = "you snugglebunny!";
+    break;
+  case 8:
+    document.querySelector(".compliment4").innerHTML = "and I don't front!";
+    break;
+  case 9:
+    document.querySelector(".compliment4").innerHTML = "fist bump. ...high five? No?";
+    break;
+  case 10:
+    document.querySelector(".compliment4").innerHTML = "according to CNN.";
+    break;
+  case 11:
+    document.querySelector(".compliment4").innerHTML = "at least, that's what I read on Breitbart.";
+    break;
+  case 12:
+    document.querySelector(".compliment4").innerHTML = "no puffy stuffs!";
+    break;
+  case 13:
+    document.querySelector(".compliment4").innerHTML = "MOOOOO!!!";
+    break;
+  case 14:
+    document.querySelector(".compliment4").innerHTML = "ain't no thing but a chicken wing.";
+    break;
+  case 15:
+    document.querySelector(".compliment4").innerHTML = "believe it or be damned!";
+    break;
+  case 16:
+    document.querySelector(".compliment4").innerHTML = "and the cows come runnin'.";
+    break;
+  case 17:
+    document.querySelector(".compliment4").innerHTML = "say it again!";
+    break;
+  case 18:
+    document.querySelector(".compliment4").innerHTML = "YEE-HAWWWW!";
+    break;
+  default:
+    console.log("Error: " + arr[3]);
+}
+
+window.addEventListener('load', (event) => {
+  console.log('page is fully loaded');
+  console.log(arr);
+});
